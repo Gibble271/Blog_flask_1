25b30914dd9f1201b52f7497a7232d706d3bb8c0
James Lim <limchangthe@yahoo.com>
2021-01-03 11:56:17 -0800

    rearrange routes so it is in alphabetic order.

--- blog/routes.py
+++ blog/routes.py
@@ -13,21 +13,6 @@ def initDB(*args, **kwargs):
 def about():
     return render_template('about.html', title='About')
 
-
-# In register, be sure to add the send email to new user for verification.
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        new_user = User(email = form.email.data, username = form.username.data, first_name=form.first_name.data,
-                        last_name = form.last_name.data)
-        new_user.set_password_hash(form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        flash(f'User {form.username.data} has been created!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form = form)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -46,3 +31,18 @@ def logout():
     logout_user()
     flash(f'You have successfully logged out')
     return redirect(url_for('about'))
+
+# In register, be sure to add the send email to new user for verification.
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(email = form.email.data, username = form.username.data, first_name=form.first_name.data,
+                        last_name = form.last_name.data)
+        new_user.set_password_hash(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f'User {form.username.data} has been created!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form = form)
+
