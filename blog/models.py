5b70110e439ef38b3eef26bb35cdaab144199232
James Lim <limchangthe@yahoo.com>
2021-01-09 12:39:39 -0800

    added the comment model

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,15 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class Comments(db.Model):
+    #variables
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(120), nullable=False)
+
+    #relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    discussion_id = db.Column(db.Integer, db.ForeignKey('discussion.id'), nullable=False)
+
 class Community(db.Model):
     #variables
     id = db.Column(db.Integer, primary_key=True)
